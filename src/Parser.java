3b097f40e54bfb4f324fe036ce56de691a739763
Youssef W. Soliman <ywsoliman@gmail.com>
2021-11-11 18:46:08 +0200

    Initial Commit

---
+++ src/Parser.java
@@ -0,0 +1,20 @@
+
+public class Parser {
+	String commandName;
+	String[] args;
+	
+	//This method will divide the input into commandName and args
+	//where "input" is the string command entered by the user
+	public boolean parse(String input){
+		return false;
+		
+	}
+	public String getCommandName(){
+		return commandName;
+
+	}
+	public String[] getArgs(){
+		return args;
+
+	}
+}
